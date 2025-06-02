8676381f92d45b5981b77385f579e73576581399
Jim <109880774+jimjimcbyt@users.noreply.github.com>
2025-06-02 21:32:08 +0800

    改MD檔 新增橫線

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-段 = 0
+def on_button_pressed_ab():
+    global 段
+    段 = 0
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
 轉速 = 0
-pins.analog_write_pin(AnalogPin.P0, 1023)
-pins.analog_write_pin(AnalogPin.P1, 1023)
-pins.analog_write_pin(AnalogPin.P2, 1023)
-pins.analog_write_pin(AnalogPin.P3, 1023)
+段 = 0
+pins.analog_write_pin(AnalogPin.P0, 0)
+pins.analog_write_pin(AnalogPin.P1, 0)
 
 def on_forever():
-    global 轉速
-    轉速 = 段 * 200
-basic.forever(on_forever)
-
-def on_forever2():
     global 段
     if input.temperature() >= 27:
         段 = 5
@@ -20,4 +18,13 @@ def on_forever2():
         段 = 3
     elif input.temperature() >= 24 and input.temperature() < 25:
         段 = 2
+basic.forever(on_forever)
+
+def on_forever2():
+    pins.analog_write_pin(AnalogPin.P1, 轉速)
 basic.forever(on_forever2)
+
+def on_forever3():
+    global 轉速
+    轉速 = 段 * 200
+basic.forever(on_forever3)
